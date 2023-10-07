212c809b0eea2b421d2fdc55efc2ad8c65fc4e84
nicolasviruel <nicolasviruel@gmail.com>
2023-10-07 10:37:41 -0300

    Se agregan variables de entorno al proyecto, conexion a la base de datos

--- server/database/index.js
+++ server/database/index.js
@@ -1,10 +1,11 @@
 const mysql = require("mysql");
+require('dotenv').config(); // Cargar variables de entorno
 
 const db = mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"pichones1",
-    database:"empleadoscrud"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
 });
 
 console.log("Conexión a la base de datos🌝");
